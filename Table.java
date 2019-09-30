7f0810162f1fa45987dc62a9824ff0db95aa9848
Sherley Thirumavalan <33011143+sherleyt@users.noreply.github.com>
2019-09-30 14:55:55 -0500

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+import project1.Dbms;
+public class Table{
+	ArrayList<ArrayList<String>> table;	
+	int size = 0;
+	public Table(ArrayList<String> columns) {
+		size = columns.size();
+		for(int i = 0; i < size; i++) {
+			ArrayList<String> entry;
+			table.pushback(entry);
+		}
+	}
+	
+	public insert(ArrayList<String> values) {
+		ArrayList<String> tempEntry;
+		for(int i = 0; i < size; i++) {
+			
+		}
+	}
+	
+}
