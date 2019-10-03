1c486fde3e819dacbb28391016efc9987f987a7b
sebyxtan <39785336+sebyxtan@users.noreply.github.com>
2019-10-03 13:34:24 -0500

    Update Table.java

--- finl/src/Table.java
+++ finl/src/Table.java
@@ -25,23 +25,33 @@ public class Table {
         columnDataTypes.put(name,dataType);
         return true;
     }
+    public boolean AddPrimaryKey(String name){
+        if (columnNames.contains(name)){
+            primaryKeys.add(name);
+            return true;
+        } else return false;
+    }
 
-    public boolean AddEntry(Object... values){
+    public boolean AddEntry(List<Object> values) {
 
         //Check if input can hold primary key indexes;
-        for (String key: primaryKeys)
-            if (values.length < columnNames.indexOf(key))
+        for (String key : primaryKeys){
+            if (values.size() < columnNames.indexOf(key)) {
+
                 return false;
+            }
+        }
 
         //Check if input primary keys clash with existing ones
         for (Hashtable<String,Object> entry: entries){
             boolean matching = true;
             for (String key : primaryKeys){
-                if (entry.get(key).equals(values[columnNames.indexOf(key)]))
+                if (! entry.get(key).equals(values.get(columnNames.indexOf(key))))
                     matching = false;
             }
-            if (matching)
+            if (matching) {
                 return false;
+            }
         }
 
         //Check if input matches column datatypes
@@ -49,9 +59,10 @@ public class Table {
 
         //Add entry
         Hashtable<String,Object> newEntry = new Hashtable<>();
-        for (int i = 0; i < values.length && i < columnNames.size(); i++){
-            newEntry.put(columnNames.get(i),values[i]);
+        for (int i = 0; i < values.size() && i < columnNames.size(); i++){
+            newEntry.put(columnNames.get(i),values.get(i));
         }
+        entries.add(newEntry);
         return true;
     }
 
@@ -63,5 +74,17 @@ public class Table {
     }
     public HashSet<Hashtable<String,Object>> getEntries(){
         return entries;
+    }
+    public void printer(){
+        int i = 0;
+        for(Hashtable<String,Object> temp : entries){
+            System.out.print("Entry ["+i+"]: ");
+            for(int j = 0; j<columnNames.size();j++){
+                String Column_print = columnNames.get(j);
+                System.out.print(Column_print+ "="+temp.get(Column_print)+ ", ");
+            }
+            ++i;
+            System.out.println("");
+        }
     }
 }
