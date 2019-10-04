7da3ad8a9c85e01e3e615d1e6af005719881099b
sebyxtan <39785336+sebyxtan@users.noreply.github.com>
2019-10-04 18:20:11 -0500

    Update Table.java

--- finl/src/Table.java
+++ finl/src/Table.java
@@ -3,19 +3,22 @@ package project1;
 import java.util.*;
 
 public class Table {
+    private String name;
     private List<String> columnNames; //Name
     private Hashtable<String,String> columnDataTypes; // DataType
     private HashSet<String> primaryKeys; //Which Names are Primary Keys
     private HashSet<Hashtable<String,Object>> entries; //List of Column-Data
 
 
-    public Table(){
+    public Table(String name){
+        this.name = name;
         columnNames = new ArrayList<>();
         columnDataTypes = new Hashtable<>();
         primaryKeys = new HashSet<>();
         entries = new HashSet<>();
     }
     public Table(Table copy){
+        this.name = copy.getName();
         columnNames = new ArrayList<>(copy.getColumnsNames());
         columnDataTypes = new Hashtable<>(copy.getDataTypes());
         primaryKeys = new HashSet<>(copy.getPrimaryKeys());
@@ -46,7 +49,6 @@ public class Table {
         //Check if input can hold primary key indexes;
         for (String key : primaryKeys){
             if (values.size() < columnNames.indexOf(key)) {
-
                 return false;
             }
         }
@@ -67,14 +69,22 @@ public class Table {
         //IMPLEMENT THIS
 
         //Add entry
+        System.out.printf("%-15s","Added Entry: ");
         Hashtable<String,Object> newEntry = new Hashtable<>();
         for (int i = 0; i < values.size() && i < columnNames.size(); i++){
             newEntry.put(columnNames.get(i),values.get(i));
+            System.out.printf("%-15s",columnNames.get(i)+ "="+values.get(i)+ ",");
         }
+        System.out.println(" to Table: " + getName());
         entries.add(newEntry);
         return true;
     }
-
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
     public List<String> getColumnsNames(){
         return columnNames;
     }
@@ -88,15 +98,17 @@ public class Table {
         return entries;
     }
     public void Print(){
+        System.out.println("\nPrinting table: " + getName());
         int i = 0;
         for(Hashtable<String,Object> temp : entries){
             System.out.printf("%-15s","Entry ["+i+"]: ");
             for(int j = 0; j<columnNames.size();j++){
                 String Column_print = columnNames.get(j);
-                System.out.printf("%-20s",Column_print+ "="+temp.get(Column_print)+ ",");
+                System.out.printf("%-15s",Column_print+ "="+temp.get(Column_print)+ ",");
             }
             ++i;
             System.out.println("");
         }
+        System.out.println("");
     }
 }
