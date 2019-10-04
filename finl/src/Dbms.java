df5fe072a999117031c75ad071d42feaacae6232
sebyxtan <39785336+sebyxtan@users.noreply.github.com>
2019-10-03 19:05:39 -0500

    Update Dbms.java

--- finl/src/Dbms.java
+++ finl/src/Dbms.java
@@ -4,27 +4,40 @@ import java.util.*;
 
 public class Dbms {
     private Hashtable<String,Table> tables;
+    private Table selectTable;
 
     public Dbms(){
         tables = new Hashtable<>();
-
+        selectTable = new Table();
     }
 
     public boolean AddTable(String name,Table table){
         tables.put(name, table);
 
+        /* //Debugging table adding
         System.out.print("Added Table \"" + name + "\": ( ");
         table.getColumnsNames().forEach((n)->System.out.print(n + "(" + table.getDataType(n) + "), "));
         System.out.println(")");
+         */
 
         return true;
     }
 
-    public void printer(String name){
-        tables.get(name).printer();
+    public void PrintTable(String name){
+        tables.get(name).Printer();
     }
 
-    public Table GetTable(String name){
+    public Table getTable(String name){
         return tables.get(name);
+    }
+    public Table getSelect(){
+        return selectTable;
+    }
+    public void setSelect(Table target){
+         selectTable = target;
+    }
+    public boolean ClearSelect(){
+        selectTable = new Table();
+        return true;
     }
 }
