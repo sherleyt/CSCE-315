627fcc9fc9b12d72283962e939c37d7ea3da1a42
spatel3456 <38338397+spatel3456@users.noreply.github.com>
2019-10-07 15:42:49 -0500

    Delete MyRulesBaseListener.java

--- MyRulesBaseListener.java
+++
@@ -1,9 +0,0 @@
-import project1.Dbms;
-public class MyRulesBaseListener extends RulesBaseListener {
-	public MyRulesBaseListener() {
-	     myDbms = new Dbms();
-	}
-	@Override public void exitShowCmd(RulesParser.ShowCmdContext ctx) {
-	     System.out.println("SHOW");
-	}
-}
