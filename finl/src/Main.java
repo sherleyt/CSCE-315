b7e2fec569239c60065a739c6557402effbc7dde
Sherley Thirumavalan <33011143+sherleyt@users.noreply.github.com>
2019-10-17 15:29:37 -0500

    Update Main.java

--- finl/src/Main.java
+++ finl/src/Main.java
@@ -45,28 +45,22 @@ public class Main {
         MovieDatabaseParser parser1 = new MovieDatabaseParser();
         MyRulesBaseListener listener = new MyRulesBaseListener();
 
-        List<Movie> moviesList = parser1.deserializeMovies("src/project1/movies.json");
-        List<Credits> creditsList = parser1.deserializeCredits("src/project1/credits.json");
+        List<Movie> moviesList = parser1.deserializeMovies("src/project1/movies_single.json");
+        List<Credits> creditsList = parser1.deserializeCredits("src/project1/credits_single.json");
 
         // create a big data base
         // create a map that maps each genre we encounter to a genre name
         List<String> lines = new ArrayList<>();   //"matrix"
-        // genre is said to be integer but it is actually a list of ints
         lines.add("CREATE TABLE movies (id INTEGER, popularity INTEGER, has_credits INTEGER) PRIMARY KEY (id);");
         lines.add("CREATE TABLE cast (m_id INTEGER, name VARCHAR(150), character_name VARCHAR(150), " +
                 "id INTEGER, credit_id VARCHAR(150)) PRIMARY KEY (credit_id);");
         lines.add("CREATE TABLE genres (movie_id INTEGER, genre_id INTEGER, genre_name VARCHAR(150), index INTEGER) PRIMARY KEY (index);");
-
-        System.out.println("here1");
 
         // adding info into the movies table
         for(int i = 0; i < moviesList.size(); i++) {
             // TODO: change the popularity
             lines.add("INSERT INTO movies VALUES FROM ("+moviesList.get(i).getId()+", "+
                     Math.round(moviesList.get(i).getPopularity()*1000)+", "+moviesList.get(i).getHasCredits()+");");
-            // create a table called g{movie_id}
-            //lines.add("CREATE TABLE g"+moviesList.get(i).getId()+" (genre_id INTEGER, genre_name VARCHAR(30)) PRIMARY KEY (genre_id);");
-//            lines.add("CREATE TABLE genres (movie_id INTEGER, genre_id INTEGER, genre_name VARCHAR(30)) PRIMARY KEY (genre_id);");
             for(int j = 0; j < moviesList.get(i).getGenres().size(); j++) {
                 int id = moviesList.get(i).getGenres().get(j).getId();
                 String name = removeSpace(moviesList.get(i).getGenres().get(j).getName());
@@ -76,13 +70,9 @@ public class Main {
         }
         lines.add("SHOW genres;");
 
-        System.out.println("here2");
-
         // adding info into the cast table
         // removing white spaces from name and character name
         for(int i = 0; i < creditsList.size(); i++) {
-            //int movie_id = Integer.parseInt(creditsList.get(i).getId());
-            // TODO: check if this fixes
             long movie_id = Long.parseLong(creditsList.get(i).getId());
             for(int j = 0; j < creditsList.get(i).getCastMember().size(); j++) {
                 String name = creditsList.get(i).getCastMember().get(j).getName();
@@ -105,49 +95,43 @@ public class Main {
             }
         }
 
+        lines.add("SHOW movies;");
 
-//        System.out.println("here2");
-//
         // To get the query and generate the SQL statements
-        int query = 4; // NEED TO GET THE VALUE FROM THE GUI
+        // TODO: get the value from the GUI
+        int query = 2;
         boolean checkthree = false;
+        boolean checktwo = false;
+        int checktwocount = 0;
         switch(query)
         {
+            // WORKS ON SINGLE MOVIE AND TWO MOVIES
             case 2:
-                //
-                // if actor exists in a movie put everyone in the movie into a table with the count on another column
-                // can use some data structure to check if present or even store everything. and then everything with
-                // the
+                checktwo = true;
+                // TODO: change the actor_name to get the response frm GUI
+                String check_actor = "Tom Hanks";
+                // TODO: get char_count frm the GUI
+                int char_count = 2;
+                checktwocount = char_count;
+                lines.add("storetemp <- select (name == \"" + removeSpace(check_actor) +"\") cast;");
+                lines.add("temp <- rename (m_id_two, name_two, character_name_two, id_two, credit_id_two) storetemp;");
+                lines.add("temp2 <- temp * cast;");
+                lines.add("temp3 <- select(name_two != name && m_id_two == m_id) temp2;");
+
                 break;
             case 3:
-                // go through everything in cast table if the person is present, find the genre table for the movie id
-                // add a new row for each genre with the movie id, the persons name, and the genre
-                // then find the most common genre of the table using the example in the slides.
                 // TODO: change the actor_name to get the response frm GUI
                 String actor_name = "Tom Hanks";
-                String tempAct = "";
-                for(int k = 0; k < actor_name.length(); k++) {
-                    if(Character.isLetterOrDigit(actor_name.charAt(k))) {
-                        tempAct = tempAct + actor_name.charAt(k);
-                    }
-                }
-                lines.add("store <- select (name == \"" + tempAct +"\") cast;");
+                lines.add("store <- select (name == \"" + removeSpace(actor_name) +"\") cast;");
                 lines.add("store2 <- store * genres;");
                 lines.add("store3 <- select (m_id == movie_id) store2;");
                 lines.add("SHOW store3;");
-                // need to multiple the store with the genres
                 checkthree = true;
                 break;
             case 4:
                 // TODO: change the character_name to get the response frm GUI
-                String character_name = "Woody (voice)"; // get actor 1 from GUI
-                String tempChar = "";
-                for(int k = 0; k < character_name.length(); k++) {
-                    if(Character.isLetterOrDigit(character_name.charAt(k))) {
-                        tempChar = tempChar + character_name.charAt(k);
-                    }
-                }
-                lines.add("store <- select (character_name == \"" + tempChar +"\") cast;");
+                String character_name = "Woody (voice)";
+                lines.add("store <- select (character_name == \"" + removeSpace(character_name) +"\") cast;");
                 lines.add("temp <- (project (name) store);");
                 lines.add("SHOW temp;");
                 break;
@@ -160,7 +144,7 @@ public class Main {
 
         //Send to antlr/listener
         for (String line : lines) {
-            System.out.println("parsing line " + line);
+            //System.out.println("parsing line " + line);
             //Actual strings
             CharStream charStream = CharStreams.fromString(line);
             RulesLexer lexer = new RulesLexer(charStream);
@@ -174,6 +158,31 @@ public class Main {
             walker.walk(listener, programContext);
         }
 
+        if(checktwo) {
+            System.out.println("in check two");
+            Table temp = listener.myDbms.getTable("temp3");
+            temp.Print();
+
+            Map<String, Integer> count = new TreeMap<String, Integer>();
+
+            // to put in the names
+            for (Hashtable<String,Object> entry : temp.getEntries()) {
+                String name = entry.get("name").toString();
+                if (!count.containsKey(name)) {
+                    count.put(name, 1);
+                } else {
+                    count.put(name, count.get(name) + 1);
+                }
+            }
+
+            // To display the actors played in given number of movies with choosen actor
+            System.out.println("The actors who played " + checktwocount + " movies with choosen actor are:");
+            for(String key : count.keySet()) {
+                if(count.get(key) == checktwocount) {
+                    System.out.println(key);
+                }
+            }
+        }
 
         if(checkthree) {
             // find the most common genre in store
@@ -199,12 +208,18 @@ public class Main {
             }
             // To display the max genres
             System.out.println("The most common genre/genres played is/are:");
-            for(Hashtable<String,Object> entry : temp.getEntries()) {
-                String genre_name = entry.get("genre_name").toString();
-                if(count.get(genre_name) == max) {
-                    System.out.println(genre_name);
+
+            for(String key : count.keySet()) {
+                if(count.get(key) == max) {
+                    System.out.println(key);
                 }
             }
+//            for(Hashtable<String,Object> entry : temp.getEntries()) {
+//                String genre_name = entry.get("genre_name").toString();
+//                if(count.get(genre_name) == max) {
+//                    System.out.println(genre_name);
+//                }
+//            }
         }
     }
 }
